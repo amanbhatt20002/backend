5e8cd268293eaeb1554595a9be3408927d34f3f7
aman bhatt <amanb3588@gmail.com>
2025-10-02 00:02:59 +0530

    add user and vedio model

--- src/app.js
+++ src/app.js
@@ -1,29 +1,19 @@
- import express from "express"
-import cors from "cors"
-import cookieParser from "cookie-parser"
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
+const app = express();
 
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
 
- const app=express()
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ limit: "16kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
 
- app.use(cors({
-    origin:process.env.CORS_ORIGIN,
-    credentials:true,
-
- }))
-
- app.use(express.json({limit:"16kb"}))
- app.use(express.urlencoded({limit:"16kb"}))
- app.use(express.static("public"))
- app.use(cookieParser())
- 
-
-
-
-
-
-
-
-
-
- export {app}
+export { app };
